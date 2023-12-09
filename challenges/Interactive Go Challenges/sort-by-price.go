5934ada3ffd754f2891724fbec0bd2ac52aeb31b
Sapan Patibandha <patibandha@hotmail.com>
2023-12-09 13:03:15 +0530

    sort by price is created

--- challenges/Interactive Go Challenges/sort-by-price.go
+++ challenges/Interactive Go Challenges/sort-by-price.go
@@ -1,31 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // Flight - a struct that
 // contains information about flights
 type Flight struct {
-  Origin string
-  Destination string
-  Price int
+	Origin      string
+	Destination string
+	Price       int
 }
 
-// SortByPrice sorts flights from highest to lowest
+type ByPrice []Flight
+
+// func (p ByPrice) Len() int {
+// 	return len(p)
+// }
+
+func (p ByPrice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p ByPrice) Less(i, j int) bool {
+	return p[i].Price > p[j].Price
+}
+
 func SortByPrice(flights []Flight) []Flight {
-  // implement
-  return nil
+	sort.Sort(ByPrice(flights))
+	return flights
 }
-
 func printFlights(flights []Flight) {
-  for _, flight := range flights {
-    fmt.Printf("Origin: %s, Destination: %s, Price: %d", flight.Origin, flight.Destination, flight.Price)
-  }
+	for _, flight := range flights {
+		fmt.Printf("Origin: %s, Destination: %s, Price: %d \n", flight.Origin, flight.Destination, flight.Price)
+	}
 }
 
 func main() {
-  // an empty slice of flights
-  var flights []Flight
+	flights := []Flight{
+		Flight{Price: 30},
+		Flight{Price: 20},
+		Flight{Price: 50},
+		Flight{Price: 1000},
+	}
 
-  sortedList := SortByPrice(flights)
-  printFlights(sortedList)
+	sortedList := SortByPrice(flights)
+	printFlights(sortedList)
 }
